ad79fbc614328a295f1c009ced16e6fdaa5f44c2
Atul kaushik <As.atul4s@gmail.com>
2025-02-06 00:36:47 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,22 @@
+gsap.from(".container",{
+    y:100,
+    duration:2,
+    Delay:1,
+    opacity:0,
+    stagger:0.4
+    
+
+})
+
+
+
+gsap.from("nav",{
+    x:100,
+    duration:2,
+    Delay:2,
+    opacity:0,
+  
+    
+
+})
+
